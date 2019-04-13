7b58716a02cdc9e3d02dc1b3e3d4633976d38810
Nikola Tasic <ntasic7@gmail.com>
2019-04-13 03:40:28 +0200

    added current folder jail mount to /src, changed tmp folder to program root

--- source/jail.cpp
+++ source/jail.cpp
@@ -16,17 +16,19 @@ using namespace std;
 
 class Jail {
 public:
-    explicit Jail(const char *);
+    explicit Jail(const char*, char* const*);
+
+    explicit Jail(const char*, const char*, char* const*);
 
     ~Jail();
 
-    inline static bool exists_stat(const char *name) {
+    inline static bool exists_stat(const char* name) {
         struct stat fileStat{};
         return stat(name, &fileStat) == 0;
     }
 
     inline static bool exists(const std::string &name) {
-        if (FILE *file = fopen(name.c_str(), "r")) {
+        if (FILE* file = fopen(name.c_str(), "r")) {
             fclose(file);
             return true;
         } else {
@@ -37,11 +39,11 @@ public:
 private:
     void setup_variables();
 
-    void setup_root();
+    void setup_root(const char* = "/");
 
-    void setup_bashrc();
+    void setup_bashrc(char*);
 
-    void setup_certs();
+    void setup_certs(char*);
 
     void setup_resolvconf();
 
@@ -51,23 +53,29 @@ private:
 
     void setup_dev();
 
-    static int start(void *);
+    void setup_src();
+
+    static int start(void*);
+
+    static int start_cmd(void*);
 
     template<typename ... Params>
     int run(Params...);
 
-    char *allocate_stack(int = 65536);
+    char* allocate_stack(int = 65536);
 
-    static void panic(const char *);
+    static void panic(const char*);
 
 
-    const char *rootfs;
-    char *cont_stack;
+    const char* rootfs;
+    const char* cmd = nullptr;
+    char* const* cmd_args;
+    char* cont_stack;
 
 };
 
 // tar -C ./rootfs/ubuntu1810-base -xf filename
-Jail::Jail(const char *rootfs) {
+Jail::Jail(const char* rootfs, char* const* args) {
     this->rootfs = rootfs;
 
     // stack for container
@@ -77,18 +85,16 @@ Jail::Jail(const char *rootfs) {
     this->setup_dev();
 
     // small bashrc for colorful terminal
-    this->setup_bashrc();
+    this->setup_bashrc(args[0]);
 
     // copy default certs to container
-    this->setup_certs();
+    this->setup_certs(args[0]);
 
     // pre-download required .deb packages to install ca-certificates
     this->setup_ssldeb();
 
-
-
     // flags to clone process tree and unix time sharing
-    int n = clone(Jail::start, this->cont_stack, CLONE_NEWPID | CLONE_NEWUTS | SIGCHLD, (void *) this);
+    int n = clone(Jail::start, this->cont_stack, CLONE_NEWPID | CLONE_NEWUTS | SIGCHLD, (void*) this);
     if (n <= 0) {
         panic("Error cloning process");
     }
@@ -97,12 +103,117 @@ Jail::Jail(const char *rootfs) {
     wait(nullptr);
 }
 
+Jail::Jail(const char* rootfs, const char* cmd, char* const* args) {
+    this->rootfs = rootfs;
+    this->cmd = cmd;
+    this->cmd_args = args;
+
+    // allocate stack for container
+    this->cont_stack = allocate_stack();
+
+    // bind /dev to container /dev
+    this->setup_dev();
+
+//    this->setup_bashrc();
+
+    this->setup_src();
+
+    // flags to clone process tree and unix time sharing
+    int n = clone(Jail::start_cmd, this->cont_stack, CLONE_NEWPID | CLONE_NEWUTS | SIGCHLD, (void*) this);
+    if (n <= 0) {
+        panic("Error cloning process");
+    }
+
+    // wait for container shell to exit
+    wait(nullptr);
+}
+
+int Jail::start(void* args) {
+    Jail* self = (Jail*) args;
+
+    self->setup_variables();
+
+    self->setup_root();
+    cout << "INFO mounted proc to /proc\n";
+
+    cout << "INFO container pid: " << getpid() << endl;
+
+    // setup nameservers for internet access
+    self->setup_resolvconf();
+
+    // install pre-downloaded ssl packages
+    self->setup_installssl();
+
+    if (self->run("/bin/bash") != 0) {
+        perror("ERROR");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+int Jail::start_cmd(void* args) {
+    Jail* self = (Jail*) args;
+    cout << "INFO root - " << self->rootfs << endl;
+    cout << "INFO cmd - " << self->cmd << endl;
+
+    self->setup_variables();
+
+    self->setup_root("/src");
+
+    cout << "INFO mounted proc to /proc\n";
+    cout << "INFO mounted current dir to /src\n";
+
+    cout << "INFO container pid: " << getpid() << endl;
+
+    // setup nameservers for internet access
+    self->setup_resolvconf();
+
+    // install pre-downloaded ssl packages
+    // self->setup_installssl();
+
+    if (execvp(((string) self->cmd).c_str(), self->cmd_args) != 0) {
+        panic("ERROR");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+template<typename ... Params>
+int Jail::run(Params ...params) {
+    char* args[] = {(char*) params..., nullptr};
+    return execvp(args[0], args);
+}
+
+
+void Jail::panic(const char* message) {
+    std::perror(message);
+    exit(-1);
+}
+
+void Jail::setup_src() {
+    system(("mkdir -p " + (string) "./rootfs/" + this->rootfs + "/src").c_str());
+    if (mount("./", ((string) "./rootfs/" + this->rootfs + "/src").c_str(), "tmpfs", MS_BIND, nullptr) < 0) {
+        panic("Unable to mount /src");
+    }
+}
+
+
+void Jail::setup_root(const char* root) {
+    chroot(((string) "./rootfs/" + this->rootfs).c_str());
+    chdir(root);
+    if (mount("proc", "/proc", "proc", 0, nullptr) < 0) {
+        panic("Unable to mount /proc");
+    }
+
+}
+
+
 void Jail::setup_variables() {
     clearenv();
     system(((string) "hostname " + this->rootfs).c_str());
     setenv("HOME", "/", 0);
     setenv("TERM", "xterm-256color", 0);
-    setenv("PATH", "/bin/:/sbin/:/usr/bin/:/usr/sbin/", 0);
+    setenv("PATH", "/bin/:/sbin/:/usr/bin/:/usr/sbin/:/src/", 0);
 }
 
 void Jail::setup_resolvconf() {
@@ -116,17 +227,19 @@ void Jail::setup_resolvconf() {
     fp.close();
 }
 
-void Jail::setup_bashrc() {
-    system(("cp ./config/.bashrc ./rootfs/" + (string) this->rootfs + "/").c_str());
+void Jail::setup_bashrc(char* ccont_root) {
+    string dir(ccont_root);
+    dir = dirname((char*) dir.c_str());
+    system(("cp " + dir + "/config/.bashrc ./rootfs/" + this->rootfs + "/").c_str());
 }
 
-void Jail::setup_root() {
-    chroot(((string) "./rootfs/" + this->rootfs).c_str());
-    chdir("/");
-    if (mount("proc", "/proc", "proc", 0, nullptr) < 0) {
-        panic("Unable to mount /proc");
+char* Jail::allocate_stack(int size) {
+    auto* stack = new(nothrow) char[size];
+    if (stack == nullptr) {
+        panic("Cannot allocate stack memory");
     }
-
+    // stack grows downwards so move ptr to the end
+    return stack + size;
 }
 
 void Jail::setup_dev() {
@@ -135,49 +248,10 @@ void Jail::setup_dev() {
     }
 }
 
-char *Jail::allocate_stack(int size) {
-    auto *stack = new(nothrow) char[size];
-    if (stack == nullptr) {
-        panic("Cannot allocate stack memory");
-    }
-    // stack grows downwards so move ptr to the end
-    return stack + size;
-}
-
-int Jail::start(void *args) {
-    Jail *self = (Jail *) args;
-    cout << "INFO root - " << self->rootfs << endl;
-
-    self->setup_variables();
-    self->setup_root();
-
-    cout << "INFO mounted proc to /proc\n";
-    cout << "INFO container pid: " << getpid() << endl;
-
-    // setup nameservers for internet access
-    self->setup_resolvconf();
-
-    // install pre-downloaded ssl packages
-    self->setup_installssl();
-    self->run("/bin/bash");
-    return EXIT_SUCCESS;
-}
-
-template<typename ... Params>
-int Jail::run(Params ...params) {
-    char *args[] = {(char *) params..., nullptr};
-    return execvp(args[0], args);
-}
-
-
-void Jail::panic(const char *message) {
-    std::perror(message);
-    exit(-1);
-}
-
-
-void Jail::setup_certs() {
-    system(("cp ./config/ca-certificates.conf ./rootfs/" + (string) this->rootfs + "/etc/").c_str());
+void Jail::setup_certs(char* ccont_root) {
+    string dir(ccont_root);
+    dir = dirname((char*) dir.c_str());
+    system(("cp " + dir + "/config/ca-certificates.conf ./rootfs/" + this->rootfs + "/etc/").c_str());
 }
 
 void Jail::setup_ssldeb() {
@@ -212,7 +286,6 @@ void Jail::setup_installssl() {
 
     // if the cmd is not available installed pre-downloaded packages
     // and re-run it to update ssl certs to make apt update possible
-//    cout << system("update-ca-certificates") << endl;
     if (system("update-ca-certificates > /dev/null")) {
         chdir("/tmp/openssl/");
         char url_libssl[] = "http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1_amd64.deb";
@@ -223,7 +296,6 @@ void Jail::setup_installssl() {
         string base_openssl(basename(url_openssl));
         string base_cacert(basename(url_cacert));
 
-        std::perror("ERROR");
         if (system(("dpkg -i " + base_libssl + "> /dev/null").c_str()) < 0) {
             panic("ERROR");
         }
@@ -247,5 +319,8 @@ Jail::~Jail() {
     // umount container /dev from outside
     system(("umount ./rootfs/" + (string) this->rootfs + "/dev").c_str());
 
+    if (this->cmd != nullptr) {
+        system(("umount ./rootfs/" + (string) this->rootfs + "/src").c_str());
+    }
 }
 
--- source/main.cpp
+++ source/main.cpp
@@ -12,37 +12,46 @@
 #include <libgen.h>
 #include "jail.cpp"
 
-void panic(const char *);
+void panic(const char*);
 
-void setup_ubuntucont(const char *);
+void setup_ubuntucont(const char*, char*);
 
 using namespace std;
 
-int main(int argc, char **args) {
+int main(int argc, char* args[]) {
     // TODO: arg rootfs picker
-    const char *rootfs = "ubuntu1810-base";
+    const char* rootfs = "ubuntu1810-base";
     cout << "parent pid: " << getpid() << endl;
 
     // download and extract minimal ubuntu-base rootfs
-    setup_ubuntucont(rootfs);
-
-    // setup and start the container in selected rootfs
-    Jail jail(rootfs);
+    cout << args[0] << endl;
+    setup_ubuntucont(rootfs, args[0]);
+    if (argc >= 2) {
+        Jail jail(rootfs, args[1], &args[1]);
+    } else {
+        // setup and start the container in selected rootfs
+        Jail jail(rootfs, &args[0]);
+    }
     return EXIT_SUCCESS;
 }
 
-void panic(const char *message) {
+void panic(const char* message) {
     std::perror(message);
     exit(EXIT_FAILURE);
 }
 
-void setup_ubuntucont(const char *bname) {
+void setup_ubuntucont(const char* bname, char* ccont_root) {
     char url[] = "http://cdimage.ubuntu.com/ubuntu-base/releases/18.10/release/ubuntu-base-18.10-base-amd64.tar.gz";
-    const char *url_base = basename(url);
+    const char* url_base = basename(url);
+    string dir(ccont_root);
+
+    dir = dirname((char*)dir.c_str());
+    dir = dir.append("/tmp");
+
     string folder("./rootfs/" + (string) bname);
 
-    if (!Jail::exists_stat("./tmp")) {
-        system("mkdir ./tmp");
+    if (!Jail::exists_stat(dir.c_str())) {
+        system(("mkdir -p " + dir).c_str());
     }
 
     // assume if there is no home folder that the rootfs doesn't exist
@@ -50,10 +59,10 @@ void setup_ubuntucont(const char *bname) {
         system(("mkdir -p " + folder).c_str());
 
         // download rootfs archive if its not present
-        if (!Jail::exists("./tmp/" + (string) url_base)) {
-            system(("wget -O ./tmp/" + (string) url_base + " " + (string) url + " -q --show-progress").c_str());
+        if (!Jail::exists(dir + "/" + url_base)) {
+            system(("wget -O " + dir + "/" + (string) url_base + " " + (string) url + " -q --show-progress").c_str());
         }
-        system(("tar xf ./tmp/" + (string) url_base + " -C " + folder + "> /dev/null").c_str());
+        system(("tar xf " + dir + "/" + (string) url_base + " -C " + folder + "> /dev/null").c_str());
     }
 }
 
